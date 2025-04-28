d6ed53d01ea9e4091bc7e31d33e5d7a856aeda15
Leki <lekisintole82@gmail.com>
2025-04-28 09:21:50 +0300

    send button styling

--- resources/views/livewire/chat.blade.php
+++ resources/views/livewire/chat.blade.php
@@ -72,7 +72,7 @@
             autofocus />
 
         <button type="submit"
-                class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition">
+                class="bg-blue-500 text-red px-4 py-2 rounded hover:bg-blue-600 transition">
             Send
         </button>
     </form>
